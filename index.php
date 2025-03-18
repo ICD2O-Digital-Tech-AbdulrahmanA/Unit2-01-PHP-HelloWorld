af221fd0d9d81397693ba2b7ee55a264c750ed1c
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2025-03-18 14:57:24 +0000

    My first PHP file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Greetings, Earth! in PHP</title>
+	</head>
+	<body>
+    <h1><?php echo 'Greetings, Earth! in PHP'; ?></h1>
+		<?php echo '<p>This is my first web page using PHP. PHP is very similar to HTML.</p>'; ?>
+	</body>
+</html>
